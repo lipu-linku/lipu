2ccd4fd70853c01262ce7c25a4757622c78db40c
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-08-09 20:30:32 +0500

    Prototype

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Entry from './Entry.svelte';
+
+	function Get(yourUrl) {
+		var Httpreq = new XMLHttpRequest();
+		Httpreq.open("GET", yourUrl, false);
+		Httpreq.send(null);
+		return Httpreq.responseText;
+	}
+	const bundle_url = "https://linku.la/jasima/data.json";
+	const bundle = JSON.parse(Get(bundle_url));
+
+	const dictionary = bundle["data"];
+	const languages = bundle["languages"];
+
+</script>
+
+<main>
+	{#each Object.entries(dictionary) as [key, word]}
+		<!--{#if word["usage_category"] != "obscure"}-->
+		<Entry {word}/>
+		<!--<entry class={word["usage_category"]}>
+			<entry_title>
+				<word>{word['word']}</word>
+				<icon_container>
+					<icon></icon>
+					<icon></icon>
+					<icon></icon>
+					<icon></icon>
+				</icon_container>
+			</entry_title>
+			<definition>{word['def']['en']}</definition>
+		</entry>-->
+
+		<!--{/if}-->
+	{/each}
+</main>
+
+<style>
+	@font-face {
+		font-family: "sitelen seli kiwen";
+		font-style: normal;
+		font-weight: 400;
+		src: url(https://raw.githubusercontent.com/lipu-linku/nasin-sitelen/main/sitelenselikiwenasuki.ttf);
+	}
+	main {
+		margin: 0 auto;
+		padding: 0 10px;
+		max-width: 840px;
+	}
+</style>
---
+++ src/Entry.svelte
@@ -0,0 +1,96 @@
+<script>
+    export let word;
+
+    const sp = word['sitelen_pona'] ? word['sitelen_pona'].split(' ')[0] : "";
+    const book_src =
+    {
+        "pu": "/assets/pu.svg",
+        "ku suli": "/assets/ku-suli.svg",
+        "ku lili": "/assets/ku-lili.svg",
+        "none": "/assets/lipu-ala.svg",
+    }[word['book']]
+</script>
+
+<entry class={word["usage_category"]}>
+    <entry_text>
+        <word>{word['word']}</word>
+        <definition>{word['def']['en']}</definition>
+    </entry_text>
+    <icon_container>
+        {#if sp}
+        <icon>
+            {sp}
+        </icon>
+        {/if}
+        <icon>
+            <img src={book_src}>
+        </icon>
+        <icon>
+            <img src="/assets/kalama.svg">
+        </icon>
+        <icon>
+            <img src="/assets/sona-mute.svg">
+        </icon>
+    </icon_container>
+</entry>
+
+<style>
+	entry {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		padding: 5px 15px;
+		border-left-width: 5px;
+		border-left-style: solid;
+		margin: 10px 0px;
+		border-top: 1px solid var(--border-color);
+	}
+	.core {
+		border-left-color: #fead76;
+	}
+	.widespread {
+		border-left-color: #f97b5d;
+	}
+	.common {
+		border-left-color: #e75362;
+	}
+	.uncommon {
+		border-left-color: #b63679;
+	}
+	.rare {
+		border-left-color: #7f2481;
+	}
+	.obscure {
+		border-left-color: #50127b;
+	}
+	word {
+		display: inline-block;
+		font-size: 150%;
+		color: var(--highlight-color);
+		font-weight: bold;
+		font-style: italic;
+	}
+	icon_container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+	}
+	icon {
+        font-family: "sitelen seli kiwen";
+        font-size: 46px;
+        text-align: center;
+		width: 40px;
+		height: 40px;
+		margin: 2px;
+		/*background-color: var(--border-color);
+		border-radius: 5px;*/
+	}
+	icon > img {
+        height: 100%;
+        width: 100%;
+        margin: auto;
+	}
+	definition {
+		display: block;
+	}
+</style>
