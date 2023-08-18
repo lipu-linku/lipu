0e3b0905a79b68997d1e3d45ed1e331ee1713b61
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-08-18 14:39:26 +0500

    Update breakpoint because of new button

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -93,7 +93,7 @@
         color: var(--bg-color);
     }
 
-    @media screen and (max-width: 640px) {
+    @media screen and (max-width: 680px) {
         nav {
             display: grid;
             grid-template-columns: 1fr 1fr;
