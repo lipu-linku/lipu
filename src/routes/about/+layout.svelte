b12cfceca05ac4d4b14c4cd51afb332e5754f934
theonlytails <theonlytails@theonlytails.com>
2023-10-28 22:43:13 +0300

    initial about page

---
+++ src/routes/about/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { Button } from "$lib/components/ui/button";
+	import BackIcon from "~icons/lucide/arrow-left";
+</script>
+
+<Button
+	class="absolute top-4 left-4"
+	href={$page.route.id === "/about" ? "/" : "/about"}
+	variant="ghost"
+	size="icon"
+>
+	<BackIcon class="h-11 w-11" />
+</Button>
+
+<main class="flex flex-col items-center justify-center gap-4 min-h-screen [&_a]:underline">
+	<slot />
+</main>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,31 +1,56 @@
 <script lang="ts">
+	import TableIcon from "~icons/lucide/table-2";
+	import RobotIcon from "~icons/mdi/robot-excited";
+	import DatabaseIcon from "~icons/lucide/database";
+	import WebIcon from "~icons/lucide/globe";
+	import DiscordIcon from "~icons/simple-icons/discord";
 </script>
 
-<div class="page_width_limiter">
-	<a class="navlink" href="/">Go back</a>
-	<h1>About Linku</h1>
-	<p>
-		Linku is a <a href="about/nimi">sheet</a>, a
-		<a href="about/ilo">bot</a>, a <a href="about/jasima">database</a> and a
-		<a href="about/lipu">website</a>, put together by kala Asi for the purpose of displaying toki
-		pona dictionary information.
-	</p>
-	<p>
-		ku data is provided by
-		<a href="http://tokipona.org/nimi_pu.txt">tokipona.org</a>.
-	</p>
-	<h2><a href="about/nimi">nimi Linku (the sheet)</a></h2>
-	<h2><a href="about/ilo">ilo Linku (the bot)</a></h2>
-	<h2><a href="about/jasima">jasima Linku (the mirror / database)</a></h2>
-	<h2><a href="about/lipu">lipu Linku (the website)</a></h2>
-	<h2>
-		<a href="https://discord.gg/A3ZPqnHHsy">Discord server (contact us there!)</a>
-	</h2>
-	<p>
-		See also <a href="sitelen">sitelen Linku</a>, a small experimental page that displays existing
-		sitelen pona, with short etymologies.
-	</p>
-</div>
+<h1 class="font-medium text-4xl">About Linku</h1>
 
-<style>
-</style>
+<p>
+	Linku is a <a href="/about/nimi">sheet</a>, a
+	<a href="/about/ilo">bot</a>, a <a href="/about/jasima">database</a> and a
+	<a href="/about/lipu">website</a>, put together by kala Asi for the purpose of displaying toki
+	pona dictionary information.
+</p>
+
+<p>
+	ku data is provided by
+	<a href="http://tokipona.org/nimi_pu.txt">tokipona.org</a>.
+</p>
+
+<ul
+	class="rounded-md p-4 list-disc flex flex-col gap-4"
+>
+	<li>
+		<span class="flex items-center gap-[1ch]">
+			<TableIcon /> <a href="/about/nimi">nimi Linku (the sheet)</a>
+		</span>
+	</li>
+	<li>
+		<span class="flex items-center gap-[1ch]">
+			<RobotIcon /> <a href="/about/ilo">ilo Linku (the bot)</a>
+		</span>
+	</li>
+	<li>
+		<span class="flex items-center gap-[1ch]">
+			<DatabaseIcon /> <a href="/about/jasima">jasima Linku (the mirror / database)</a>
+		</span>
+	</li>
+	<li>
+		<span class="flex items-center gap-[1ch]">
+			<WebIcon /> <a href="/about/lipu">lipu Linku (the website)</a>
+		</span>
+	</li>
+	<li>
+		<span class="flex items-center gap-[1ch]">
+			<DiscordIcon />
+			<a href="https://discord.gg/A3ZPqnHHsy">Discord server (contact us there!)</a>
+		</span>
+	</li>
+</ul>
+<p>
+	See also <a href="/sitelen">sitelen Linku</a>, a small experimental page that displays existing
+	sitelen pona, with short etymologies.
+</p>
