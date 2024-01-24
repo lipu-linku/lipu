abc2fec4fa6c0e60a39a4e11df13727b7eb2d6a1
theonlytails <theonlytails@theonlytails.com>
2024-01-24 14:12:17 +0200

    fix jasima link

--- src/routes/(data)/data.json/+server.ts
+++ src/routes/(data)/data.json/+server.ts
@@ -1,7 +1,9 @@
 import { json } from "@sveltejs/kit";
 
 export const GET = async () => {
-	const data = await fetch("https://linku.la/jasima/data.json").then((r) => r.json());
+	const data = await fetch(
+		"https://raw.githubusercontent.com/lipu-linku/jasima/main/data.json",
+	).then((r) => r.json());
 
 	return json(data);
 };
--- src/routes/(data)/data/+server.ts
+++ src/routes/(data)/data/+server.ts
@@ -2,7 +2,9 @@ import type { Linku } from "$lib/types";
 import { json } from "@sveltejs/kit";
 
 export const GET = async () => {
-	const data = await fetch("https://linku.la/jasima/data.json").then<Linku>((r) => r.json());
+	const data = await fetch(
+		"https://raw.githubusercontent.com/lipu-linku/jasima/main/data.json",
+	).then<Linku>((r) => r.json());
 
 	return json(data);
 };
