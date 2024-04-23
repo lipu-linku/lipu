ad9fb77b1d9406027cb2139dde20cea8a9f6f13e
TheOnlyTails <theonlytails@theonlytails.com>
2024-04-23 21:31:40 +0300

    Update +server.ts

--- src/routes/jasima/data.json/+server.ts
+++ src/routes/jasima/data.json/+server.ts
@@ -1,12 +1,9 @@
+import { client } from "@kulupu-linku/sona/client";
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async ({ fetch, setHeaders }) => {
-	const r = await fetch(
-		"https://raw.githubusercontent.com/lipu-linku/jasima/main/data.json"
-	).then(r => r.json());
-
 	setHeaders({ "Cache-Control": "s-maxage=31536000" });
 
-	return json(r);
+	return await client({ fetch }).jasima.$get();
 };
