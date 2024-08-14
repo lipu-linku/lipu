a337afeeadebafc05a2a01a7e96b79f78af0ad91
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-08-15 00:46:54 +0500

    survey: add edit response

---
+++ src/routes/wile-edit/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>lipu Linku &middot; Survey (edit response)</title>
+	<!-- 2024 survey -->
+	<meta http-equiv="refresh" content="0; url=https://docs.google.com/forms/d/e/1FAIpQLScxxw6LgonJXkyygq8L5zSsHJyCaO1D4eu7OyTrIujSpDaCXw/viewform" />
+</svelte:head>
