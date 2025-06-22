bc8da1e3b83df85f44353fe985460b742cca74bd
theonlytails <theonlytails@theonlytails.com>
2025-06-22 20:35:59 +0300

    removed copy button (i cannot figure it out)

--- src/routes/blog/posts/[slug]/+page.svelte
+++ src/routes/blog/posts/[slug]/+page.svelte
@@ -31,25 +31,35 @@
 	<p>
 		<span class="text-foreground">
 			{#if typeof post.author === "object"}
+				{@const author = post.author}
 				<svelte:element
 					this={post.author.blog ? "a" : "span"}
 					href={post.author.blog || undefined}
 					target={post.author.blog ? "_blank" : undefined}
 					rel={post.author.blog ? "noopener noreferrer author" : undefined}
+					class="align-"
 				>
-					<Avatar class="align-middle size-6 inline-block">
-						<AvatarImage src={post.author.avatar_url} alt="{post.author.name} avatar" />
-						<AvatarFallback>
-							{post.author.name.substring(0, 2).toLocaleUpperCase()}
-						</AvatarFallback>
+					<Avatar class="align-middle me-1 size-6 inline-block">
+						{#snippet child({ props })}
+							<span {...props}>
+								<AvatarImage
+									class="size-6 rounded-full"
+									src={author.avatar_url}
+									alt="{author.name} avatar"
+								/>
+								<AvatarFallback>
+									{author.name.substring(0, 2).toLocaleUpperCase()}
+								</AvatarFallback>
+							</span>
+						{/snippet}
 					</Avatar>
-					<span class="align-middle">{post.author.name}</span>
+					{post.author.name}
 				</svelte:element>
 			{:else}
-				<span class="align-middle">{post.author}</span>
+				{post.author}
 			{/if}
 		</span>
-		<span class="align-middle">
+		<span>
 			on <time datetime={post.date.toISOString()}>
 				{post.date.toLocaleDateString("en", { dateStyle: "long" })}
 			</time>
@@ -72,64 +82,3 @@
 	<title>lipu Linku &middot; {post.title}</title>
 	{@html head}
 </svelte:head>
-
-<style>
-	@reference "../../../../app.css";
-
-	:global {
-		.markdown-article {
-			.callout {
-				@apply rounded-e-lg border-s-2 py-4 ps-6 text-neutral-200;
-
-				border-color: var(--callout-color);
-				background-color: color-mix(in oklab, var(--callout-color) 40%, transparent);
-
-				&[data-callout="note"] {
-					--callout-color: var(--color-blue-400);
-				}
-
-				&[data-callout="warning"] {
-					--callout-color: var(--color-yellow-300);
-				}
-
-				&[data-callout="error"],
-				&[data-callout="danger"],
-				&[data-callout="caution"] {
-					--callout-color: var(--color-red-300);
-				}
-
-				&[data-callout="tip"] {
-					--callout-color: var(--color-green-300);
-				}
-
-				&[data-collapsible="true"] {
-					@apply transition-[height];
-
-					.callout-title .callout-fold-icon {
-						@apply transition-transform;
-					}
-
-					&:open .callout-title .callout-fold-icon {
-						@apply rotate-180;
-					}
-				}
-
-				.callout-title {
-					@apply flex items-center gap-2 text-xl font-semibold;
-				}
-
-				.callout-content {
-					@apply pt-4 italic;
-				}
-			}
-
-			figure {
-				@apply flex flex-col items-center justify-center gap-2 overflow-x-auto;
-
-				figcaption {
-					@apply text-muted-foreground text-sm italic;
-				}
-			}
-		}
-	}
-</style>
--- svelte.config.js
+++ svelte.config.js
@@ -2,29 +2,24 @@ import { mdsvex } from "mdsvex";
 import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { escapeSvelte } from "mdsvex";
-import { createHighlighter } from "shiki";
+import { codeToHtml } from "shiki";
 import remarkEmoji from "remark-emoji";
 import remarkGfm from "remark-gfm";
 import rehypeCallouts from "rehype-callouts";
 import rehypeFigure from "rehype-figure";
 import rehypeSlug from "rehype-slug";
 
-const highlighter = await createHighlighter({
-	themes: ["material-theme-palenight"],
-	langs: ["javascript", "typescript", "json", "python"],
-});
-
 const config = {
 	preprocess: [
 		vitePreprocess(),
 		mdsvex({
-			layout: "$lib/components/markdown/layout.svelte",
+			layout: "./src/lib/components/markdown/layout.svelte",
 			highlight: {
 				highlighter: async (code, lang = "text") => {
 					const html = escapeSvelte(
-						highlighter.codeToHtml(code, { lang, theme: "material-theme-palenight" }),
+						await codeToHtml(code, { lang, theme: "material-theme-palenight" }),
 					);
-					return `<Components.pre code={\`${code}\`}>{@html \`${html}\`}</Components.pre>`;
+					return `<Components.pre>{@html \`${html}\`}</Components.pre>`;
 				},
 			},
 			remarkPlugins: [remarkEmoji, remarkGfm],
