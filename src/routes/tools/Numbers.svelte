8c7033078034f41ab698ec1ccebed8fbac51a7bb
maf <Programmer00001H@gmail.com>
2025-11-25 17:43:57 +0800

    prevent pu styles from hanging the browser (#81)
    Co-authored-by: Shachar Zidon <theonlytails@theonlytails.com>

--- src/routes/tools/Numbers.svelte
+++ src/routes/tools/Numbers.svelte
@@ -21,6 +21,7 @@
 		if (from < 0 || !Number.isInteger(from))
 			return { type: "err", message: `${from} cannot be represented using this system!` };
 		if (from === 0) return { type: "ok", value: "ala" };
+		if (from >= 1e5) return { type: "err", message: `${from} is too large in this system!` };
 
 		const words = {
 			ale: 100,
